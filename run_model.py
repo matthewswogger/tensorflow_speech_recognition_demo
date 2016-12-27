1a1fca90e732b210595bbd2b6f675637242f12d6
Matthew Swogger <matthewswogger@yahoo.com>
2016-12-27 14:19:12 -0600

    model evaluation

--- run_model.py
+++ run_model.py
@@ -1,63 +1,55 @@
 from __future__ import division, print_function, absolute_import
-import tflearn
+import tflearn as tf
 import speech_data
-import tensorflow as tf
+import numpy as np
+from sklearn.cross_validation import train_test_split
+
+def score_model(X, y):
+    y_predicted = np.array(model.predict(X))
+    bool_arr = np.argmax(y_predicted,axis=1) == np.argmax(np.array(y),axis=1)
+    bool_sum = np.sum(bool_arr)
+    return ('model accuracy: {}'.format(round(float(bool_sum)/bool_arr.shape[0],2)))
+
 
 LEARNING_RATE = 0.0001
-# training_iters = 300000  # steps
 BATCH_SIZE = 64
-
 WIDTH = 20  # mfcc features
 HEIGHT = 80  # (max) length of utterance
 CLASSES = 10  # digits
 
-batch = word_batch = speech_data.mfcc_batch_generator(BATCH_SIZE)
-# batch = speech_data.mfcc_batch_generator(2400)
-X, Y = next(batch)
-trainX, trainY = X, Y
-testX, testY = X, Y #overfit for now
+data_set = speech_data.mfcc_batch_generator(2400)
+X, Y = next(data_set)
+X, Y = np.array(X), np.array(Y)
 
+# get train, test, validation split
+X_train_val, X_test, y_train_val, y_test = train_test_split(X, Y, test_size=0.2,
+                                                                 random_state=0)
+X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val,
+                                                  test_size=0.2, random_state=0)
 # Network building
-net = tflearn.input_data([None, WIDTH, HEIGHT])
-net = tflearn.lstm(net, 128, dropout=0.8)
-net = tflearn.fully_connected(net, CLASSES, activation='softmax')
-net = tflearn.regression(net, optimizer='adam', learning_rate=LEARNING_RATE,
+net = tf.input_data([None, WIDTH, HEIGHT])
+net = tf.lstm(net, 128, dropout=0.8)
+net = tf.fully_connected(net, CLASSES, activation='softmax')
+net = tf.regression(net, optimizer='adam', learning_rate=LEARNING_RATE,
                                                 loss='categorical_crossentropy')
+model = tf.DNN(net, tensorboard_verbose=0)
 
 # Training
 
-# add this "fix" for tensorflow version errors
-# col = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
-# for x in col:
-#     tf.add_to_collection(tf.GraphKeys.VARIABLES, x )
+# model.load("saved_model/epoch_2000.tfl")
 
-
-model = tflearn.DNN(net, tensorboard_verbose=0)
-
-# Training
 # EPOCHS = 20
-# epochs_performed = 0
-# for _ in xrange(100):
+# epochs_performed = 2000
+# for _ in xrange(50):
 #     # Fit model
-#     model.fit(trainX, trainY, n_epoch=EPOCHS, validation_set=(testX, testY),
+#     model.fit(X_train, y_train, n_epoch=EPOCHS, validation_set=(X_val, y_val),
 #                                         show_metric=True, batch_size=BATCH_SIZE)
 #     # Save model
 #     epochs_performed += 20
 #     model_name = "saved_model/epoch_{}.tfl".format(epochs_performed)
 #     model.save(model_name)
 
-model.load("saved_model/epoch_2000.tfl")
-import numpy as np
-_y = np.array(model.predict(X))
-Y = np.array(Y)
-bool_arr = np.all(np.array(_y) == np.array(Y), axis=1)
-print (float(np.sum(bool_arr))/bool_arr.shape[0])
+# model evaluation
+model.load("saved_model/epoch_3000.tfl")
 
-
-
-
-
-
-
-
-#
+print (score_model(X_test, y_test))
